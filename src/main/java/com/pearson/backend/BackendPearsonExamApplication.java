0696e1ac8004e018b54688043311f2a366e1c202
UdithaIshan <uditha.ishan34@gmail.com>
2021-08-05 01:35:43 +0530

    Project initialized

---
+++ src/main/java/com/pearson/backend/BackendPearsonExamApplication.java
@@ -0,0 +1,13 @@
+package com.pearson.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPearsonExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPearsonExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pearson/backend/BackendPearsonExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pearson.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPearsonExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

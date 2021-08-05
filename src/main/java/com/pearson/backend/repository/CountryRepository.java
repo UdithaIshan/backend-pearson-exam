603da7cc7e77c3b5b83a162c030977374ca369ac
UdithaIshan <uditha.ishan34@gmail.com>
2021-08-05 23:41:52 +0530

    repository created

---
+++ src/main/java/com/pearson/backend/repository/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.pearson.backend.repository;
+
+import com.pearson.backend.model.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
+

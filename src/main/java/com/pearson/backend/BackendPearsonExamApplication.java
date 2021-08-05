2bbfeb5cc25486fe51f9c6c55cdeacddba195570
UdithaIshan <uditha.ishan34@gmail.com>
2021-08-05 21:05:31 +0530

    initial data loader created

--- src/main/java/com/pearson/backend/BackendPearsonExamApplication.java
+++ src/main/java/com/pearson/backend/BackendPearsonExamApplication.java
@@ -1,7 +1,17 @@
 package com.pearson.backend;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pearson.backend.model.Country;
+import com.pearson.backend.repository.CountryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @SpringBootApplication
 public class BackendPearsonExamApplication {
@@ -10,4 +20,20 @@ public class BackendPearsonExamApplication {
 		SpringApplication.run(BackendPearsonExamApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(CountryRepository countryRepository) {
+		return args -> {
+			// read json and write to db
+			ObjectMapper mapper = new ObjectMapper();
+			TypeReference<List<Country>> typeReference = new TypeReference<List<Country>>(){};
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/countries.json");
+			try {
+				List<Country> users = mapper.readValue(inputStream,typeReference);
+				countryRepository.saveAll(users);
+				System.out.println("Data saved");
+			} catch (IOException e){
+				System.out.println("Unable to save: " + e.getMessage());
+			}
+		};
+	}
 }
